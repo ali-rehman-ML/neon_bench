396b47bd90a0c7ca92a2fed32eaf5a3d8123923d
ali-rehman-ML <alee.rehmaann@gmail.com>
2025-01-10 16:15:55 +0500

    matmul_bench

--- build/CMakeFiles/UpdateYAML.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/UpdateYAML"
-  "static_build"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/UpdateYAML.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/matmul_4x4.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libmatmul_4x4.a"
-)
